21edc9edd4fafe2c8b41915d44bba6605853e992
Damisi Babalola <dami600bab@gmail.com>
2022-08-11 12:18:38 +0100

    homepage

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes:[
+      {
+        myTheme: {
+          primary: "rgba(128,0,128,0.8)" 
+        }
+      }
+    ]
+  },
+
+  theme: {
+    extend: {
+      colors: {
+        primaryColour: "rgba(128,0,128,0.8)"
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+}
