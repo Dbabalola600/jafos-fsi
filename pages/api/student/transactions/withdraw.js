a00e3236e0879c12c3e5507596c4f074d14be58f
Damisi Babalola <71076853+Dbabalola600@users.noreply.github.com>
2023-03-18 00:27:08 +0100

    Update withdraw.js

--- pages/api/student/transactions/withdraw.js
+++ pages/api/student/transactions/withdraw.js
@@ -1,6 +1,6 @@
 import connectMongo from "../../../../utils/connectMongo";
 import Student from "../../../../model/Student/StudentModel";
-import Creder from "../../../../model/Creder/creder";
+import Creder from "../../../../model/Creder/Creder";
 import TransferHistory from "../../../../model/Transactions/TransferHistory";
 
 
