798e9082d747015c59b63b2a9a3e6808f9fe718d
mac <dami600bab@gmail.com>
2023-06-03 22:35:17 +0100

    create account error handled

--- pages/api/admin/student/createStudent.js
+++ pages/api/admin/student/createStudent.js
@@ -12,27 +12,37 @@ import Student from "../../../../model/Student/StudentModel";
  * @param {import('next').NextApiRequest} req
  * @param {import('next').NextApiResponse} res
  */
- export default async function addStudent(req, res) {
-    try {
-      console.log('CONNECTING TO MONGO');
-      await connectMongo();
-      console.log('CONNECTED TO MONGO');
-      
-      console.log('CREATING DOCUMENT');
-      const {firstname, lastname, email, password, matricno} = JSON.parse(req.body)
-      // const hashpassword = await bcrypt.hash(password, 10)
+export default async function addStudent(req, res) {
+  try {
+    console.log('CONNECTING TO MONGO');
+    await connectMongo();
+    console.log('CONNECTED TO MONGO');
+
+    console.log('CREATING DOCUMENT');
+    const { firstname, lastname, email, password, matricno } = JSON.parse(req.body)
+    // const hashpassword = await bcrypt.hash(password, 10)
+
+    const isUser = Student.find({ matricno: matricno })
+
+    if (isUser[0] === undefined) {
+
       const student = await Student.create({
         firstname,
         lastname,
         email,
-        password, 
+        password,
         matricno
       });
       console.log('CREATED STUDENT');
-  
+
       res.json({ student });
-    } catch (error) {
-      console.log(error);
-      res.json({ error });
+
+    } else {
+      res.status(256).json({ message: "user exists" })
     }
+
+  } catch (error) {
+    console.log(error);
+    res.json({ error });
   }
+}
--- pages/api/student/createAccount.js
+++ pages/api/student/createAccount.js
@@ -11,23 +11,35 @@ export default async function addStudent(req, res) {
     console.log('CONNECTING TO MONGO');
     await connectMongo();
     console.log('CONNECTED TO MONGO');
-    
-    console.log('CREATING DOCUMENT');
-    const {firstname, lastname, email, password, matricno} = JSON.parse(req.body)
-    // const hashpassword = await bcrypt.hash(password, 10)
-    const student = await Student.create({
-      firstname,
-      lastname,
-      email,
-      password, 
-      matricno
-    });
-    console.log('CREATED STUDENT');
 
-    res.json({ student });
-  }else {
-   return res.status(400).json({
-    message:"wrong request"
-   })
+    console.log('CREATING DOCUMENT');
+    const { firstname, lastname, email, password, matricno } = JSON.parse(req.body)
+    // const hashpassword = await bcrypt.hash(password, 10)
+
+    const User = await Student.find({ matricno: matricno })
+
+    console.log(User[0])
+
+    if (User[0] === undefined) {
+      const student = await Student.create({
+        firstname,
+        lastname,
+        email,
+        password,
+        matricno
+      });
+      console.log('CREATED STUDENT');
+
+      res.status(200).json({ student });
+
+    } else {
+      res.status(256).json({ message: "user exists" })
+    }
+
+
+  } else {
+    return res.status(400).json({
+      message: "wrong request"
+    })
   }
 }
